4e2f8aae61480d130dfed4ab15dfd1fada15a3d6
halo <dongjin778@naver.com>
2025-10-07 13:26:43 +0900

    chore : Post 파일 위치 변경으로 인한 import 수정

--- src/main/java/springboot/kakao_boot_camp/domain/comment/dto/Comment.java
+++ src/main/java/springboot/kakao_boot_camp/domain/comment/dto/Comment.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import springboot.kakao_boot_camp.domain.user.entity.User;
+import springboot.kakao_boot_camp.domain.post.entity.Post;
 
 import java.time.LocalDateTime;
 
