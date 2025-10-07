fe223c014efbe4f1083f6f83cde220d7a1e5d4fa
halo <dongjin778@naver.com>
2025-10-07 13:25:18 +0900

    feat : PostController에 Post 조회(Get) API 추가

--- src/main/java/springboot/kakao_boot_camp/domain/post/controller/PostController.java
+++ src/main/java/springboot/kakao_boot_camp/domain/post/controller/PostController.java
@@ -1,14 +1,26 @@
 package springboot.kakao_boot_camp.domain.post.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springboot.kakao_boot_camp.domain.post.dto.PostDtos.*;
+import springboot.kakao_boot_camp.global.api.ApiResponse;
+import springboot.kakao_boot_camp.global.api.SuccessCode;
 
 @RestController
 @RequestMapping("/api/v1/post")
 public class PostController {
 
+    private final PostService postService;
+
     @GetMapping
-    public ResponseEntity<PostRes>
+    public ResponseEntity<ApiResponse<PostRes>> getPost(@PathVariable Long postId){
+        PostRes postRes= postService.getPost(postId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)      // StateLine에 200 전송
+                .body(ApiResponse.success(SuccessCode.POST_CREATE_SUCCESS, postRes));
+    }
 
 
 
