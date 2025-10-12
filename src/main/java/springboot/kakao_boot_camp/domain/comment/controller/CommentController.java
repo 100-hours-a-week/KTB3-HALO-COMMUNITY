ea6ceb6df3775eaa6eee023b9678bc5c352e909c
halo <dongjin778@naver.com>
2025-10-12 22:17:13 +0900

    feat(comment): 댓글 삭제 및 수정 성공 컨트롤러 구현

--- src/main/java/springboot/kakao_boot_camp/domain/comment/controller/CommentController.java
+++ src/main/java/springboot/kakao_boot_camp/domain/comment/controller/CommentController.java
@@ -26,6 +26,7 @@ public class CommentController {
                 .body(ApiResponse.success(SuccessCode.COMMENT_CREATE_SUCCESS, res));
     }
 
+
     // -- R --
     @GetMapping("/posts/{postId}/comments")
     public ResponseEntity<ApiResponse<CommentListRes>> getCommentList(@PathVariable Long postId, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size) {
@@ -36,11 +37,32 @@ public class CommentController {
         }
 
         return ResponseEntity.ok(ApiResponse.success(SuccessCode.COMMENT_LIST_READ_SUCCESS, res));
-}
+    }
     @GetMapping("/comments/{commentId}")
     public ResponseEntity<ApiResponse<CommentDetailRes>> getCommentDetail(@PathVariable Long commentId) {
         CommentDetailRes res = commentService.getCommentDetail(commentId);
         return ResponseEntity.ok(ApiResponse.success(SuccessCode.COMMENT_READ_SUCCESS, res));
+    }
+
+
+    // -- U --
+    @PatchMapping("/comments/{commentId}")
+    public ResponseEntity<ApiResponse<CommentUpdateRes>> updateComment(
+            @PathVariable Long commentId,
+            @RequestBody CommentUpdateReq req
+    ) {
+        CommentUpdateRes res = commentService.updateComment(commentId, req);
+        return ResponseEntity.ok(ApiResponse.success(SuccessCode.COMMENT_UPDATE_SUCCESS, res));
+    }
+
+
+    // -- D --
+    @DeleteMapping("/comments/{commentId}")
+    public ResponseEntity<ApiResponse<CommentDeleteRes>> deleteComment(
+            @PathVariable Long commentId
+    ) {
+        CommentDeleteRes res = commentService.deleteComment(commentId);
+        return ResponseEntity.ok(ApiResponse.success(SuccessCode.COMMENT_DELETE_SUCCESS, res));
     }
 
 
