2b9fd72bc541ace7851a19ead96f3f4a3bef0982
halo <dongjin778@naver.com>
2025-10-12 20:47:38 +0900

    chore(global) : SuccessCode 가독성 개선

--- src/main/java/springboot/kakao_boot_camp/global/api/SuccessCode.java
+++ src/main/java/springboot/kakao_boot_camp/global/api/SuccessCode.java
@@ -17,14 +17,15 @@ public enum SuccessCode {
     GET_USERS_SUCCESS(HttpStatus.OK, "사용자 목록 조회를 성공하였습니다."),
 
     // -- Post --
-    POST_DETAIL_READ_SUCCESS(HttpStatus.OK, "게시글 조회를 성공하였습니다."),
-    POST_LIST_READ_SUCCESS(HttpStatus.OK, "게시글 목록을 성공적으로 조회하였습니다."),
-
     POST_CREATE_SUCCESS(HttpStatus.CREATED, "게시글을 성공적으로 등록하였습니다."),
-
+    POST_LIST_READ_SUCCESS(HttpStatus.OK, "게시글 목록을 성공적으로 조회하였습니다."),
+    POST_DETAIL_READ_SUCCESS(HttpStatus.OK, "게시글 조회를 성공하였습니다."),
     POST_UPDATE_SUCCESS(HttpStatus.OK, "게시글을 성공적으로 수정하였습니다."),
+    POST_DELETE_SUCCESS(HttpStatus.OK, "게시글을 성공적으로 삭제하였습니다."),
 
-    POST_DELETE_SUCCESS(HttpStatus.OK, "게시글을 성공적으로 삭제하였습니다.");
+    // -- Comment --
+    COMMENT_CREATE_SUCCESS(HttpStatus.CREATED, "게시글을 성공적으로 등록하였습니다.");
+
 
 
     private final HttpStatus status;
