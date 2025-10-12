e62055ba6f621913166600636ec7434078f1b99f
halo <dongjin778@naver.com>
2025-10-12 13:59:38 +0900

    fix : ErrorCode status 필드 제거 status와 message를 하나로 묶을 필요가 없다고 판단

--- src/main/java/springboot/kakao_boot_camp/global/api/ErrorCode.java
+++ src/main/java/springboot/kakao_boot_camp/global/api/ErrorCode.java
@@ -11,12 +11,12 @@ public enum ErrorCode {
 
     // -- Auth
     // 1. 회원가입
-    INVALID_REQUEST(400,  "잘못된 요청입니다."),   // 400
-    DUPLICATE_EMAIL(409,  "중복된 이메일 입니다."), // 409 회원가입
+    INVALID_REQUEST("잘못된 요청입니다."),   // 400
+    DUPLICATE_EMAIL("중복된 이메일 입니다."), // 409 회원가입
 
 
     // 2. 로그인
-    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED.value(), "아이디 또는 비밀번호가 잘 못 되었습니다.") ,    // 계정 존재 여부를 숨기기 때문에 401이 맞다
+    AUTHENTICATION_FAILED("아이디 또는 비밀번호가 잘 못 되었습니다.") ,    // 계정 존재 여부를 숨기기 때문에 401이 맞다
 
 
 
@@ -25,10 +25,9 @@ public enum ErrorCode {
     // -- User --
 
     // -- Post --
-    POST_NOT_FOUND(404, "해당 게시글을 찾을 수 없습니다.");
+    POST_NOT_FOUND("해당 게시글을 찾을 수 없습니다.");
 
 
-    private final int status;
     private final String message;
 
 
